8be3254b79463e7374610ffdab75608e1e08a1b9
Carlos Eduardo Arango Gutierrez <arangogutierrez@gmail.com>
2025-03-06 16:03:47 +0100

    Bump Version to V25.3.0 (#265)
    Signed-off-by: Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -18,7 +18,7 @@ MODULE := github.com/NVIDIA/$(DRIVER_NAME)
 
 REGISTRY ?= nvcr.io/nvidia/cloud-native
 
-VERSION  ?= v25.2.0
+VERSION  ?= v25.3.0
 
 # vVERSION represents the version with a guaranteed v-prefix
 vVERSION := v$(VERSION:v%=%)
