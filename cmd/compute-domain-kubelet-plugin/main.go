5cb0b73d3628bb7d07caed9ba3ad45e26768d993
Swati Gupta <swatig@nvidia.com>
2025-08-22 01:21:44 +0000

    Update kubelet plugin signal handling on shutdown
    Signed-off-by: Swati Gupta <swatig@nvidia.com>

--- cmd/compute-domain-kubelet-plugin/main.go
+++ cmd/compute-domain-kubelet-plugin/main.go
@@ -18,6 +18,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"os/signal"
@@ -161,7 +162,7 @@ func newApp() *cli.App {
 				clientsets: clientSets,
 			}
 
-			return StartPlugin(ctx, config)
+			return RunPlugin(ctx, config)
 		},
 		Version: info.GetVersionString(),
 	}
@@ -175,8 +176,8 @@ func newApp() *cli.App {
 	return app
 }
 
-// StartPlugin initializes and runs the compute domain kubelet plugin.
-func StartPlugin(ctx context.Context, config *Config) error {
+// RunPlugin initializes and runs the compute domain kubelet plugin.
+func RunPlugin(ctx context.Context, config *Config) error {
 	// Create the plugin directory
 	err := os.MkdirAll(config.DriverPluginPath(), 0750)
 	if err != nil {
@@ -202,28 +203,29 @@ func StartPlugin(ctx context.Context, config *Config) error {
 		return fmt.Errorf("path for cdi file generation is not a directory: '%v'", config.flags.cdiRoot)
 	}
 
-	// Setup signal handling for graceful shutdown
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
-	// Create a cancellable context for cleanup
-	var driver *driver
-	ctx, cancel := context.WithCancel(ctx)
-	defer func() {
-		cancel()
-		if err := driver.Shutdown(); err != nil {
-			klog.Errorf("Unable to cleanly shutdown driver: %v", err)
-		}
-	}()
+	ctx, stop := signal.NotifyContext(ctx, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	defer stop()
 
 	// Create and start the driver
-	driver, err = NewDriver(ctx, config)
+	driver, err := NewDriver(ctx, config)
 	if err != nil {
 		return fmt.Errorf("error creating driver: %w", err)
 	}
 
-	// Wait for shutdown signal
-	<-sigs
+	<-ctx.Done()
+	// restore default signal behavior as soon as possible in case graceful
+	// shutdown gets stuck.
+	stop()
+	if err := ctx.Err(); err != nil && !errors.Is(err, context.Canceled) {
+		// A canceled context is the normal case here when the process receives
+		// a signal. Only log the error for more interesting cases.
+		klog.Errorf("error from context: %v", err)
+	}
+
+	err = driver.Shutdown()
+	if err != nil {
+		klog.Errorf("unable to cleanly shutdown driver: %v", err)
+	}
 
 	return nil
 }
