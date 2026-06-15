kubernetes-sigs/dra-driver-nvidia-gpu

Adding Unprepare state in checkpointing for compute domain to handle stale prepare call
Signed-off-by: Vishesh Tanksale <vtanksale@nvidia.com>

cmd/compute-domain-kubelet-plugin/cleanup.go
@@ -33,9 +33,11 @@ import (
 
 const (
 	ResourceClaimCleanupInterval = 10 * time.Minute
+	UnpreparedClaimEntryTTL      = 2 * ErrorRetryMaxTimeout
 )
 
 type TypeUnprepCallable = func(ctx context.Context, claimRef kubeletplugin.NamespacedObject) (bool, error)
+type TypeExpiredEntryCleanupCallable = func(ctx context.Context, now time.Time, ttl time.Duration) (int, error)
 
 type CheckpointCleanupManager struct {
 	waitGroup     sync.WaitGroup
@@ -44,7 +46,8 @@ type CheckpointCleanupManager struct {
 	devicestate   *DeviceState
 	draclient     *draclient.Client
 
-	unprepfunc TypeUnprepCallable
+	unprepfunc            TypeUnprepCallable
+	expiredEntryCleanupFn TypeExpiredEntryCleanupCallable
 }
 
 func NewCheckpointCleanupManager(s *DeviceState, client *draclient.Client) *CheckpointCleanupManager {
@@ -58,10 +61,11 @@ func NewCheckpointCleanupManager(s *DeviceState, client *draclient.Client) *Chec
 	}
 }
 
-func (m *CheckpointCleanupManager) Start(ctx context.Context, unprepfunc TypeUnprepCallable) error {
+func (m *CheckpointCleanupManager) Start(ctx context.Context, unprepfunc TypeUnprepCallable, expiredEntryCleanupFn TypeExpiredEntryCleanupCallable) error {
 	ctx, cancel := context.WithCancel(ctx)
 	m.cancelContext = cancel
 	m.unprepfunc = unprepfunc
+	m.expiredEntryCleanupFn = expiredEntryCleanupFn
 
 	m.waitGroup.Add(1)
 	go func() {
@@ -91,10 +95,10 @@ func (m *CheckpointCleanupManager) Stop() error {
 
 // cleanup() is the high-level cleanup routine run once upon plugin startup and
 // then periodically. It gets all claims in PrepareStarted state from the
-// current checkpoint, and runs `unprepareIfStale()` for each of them. Each
-// invocation of `cleanup()` and each invocation of `unprepareIfStale()` is
-// best-effort: errors do not need to be propagated (but are expected to be
-// properly logged).
+// current checkpoint, and runs `unprepareIfStale()` for each of them. It also
+// removes expired UnprepareCompleted entries. Each invocation of `cleanup()`
+// and each invocation of `unprepareIfStale()` is best-effort: errors do not
+// need to be propagated (but are expected to be properly logged).
 //
 // Note: This function does not acquire DeviceState lock when reading the checkpoint.
 // The lock is not needed because:
@@ -103,8 +107,8 @@ func (m *CheckpointCleanupManager) Stop() error {
 //     means we catch it on the next periodic iteration.
 //  2. We validate each candidate against the API server (the authoritative source of
 //     truth for whether a claim is truly stale), not the local checkpoint state.
-//  3. The actual checkpoint mutation happens in nodeUnprepareResource(), which properly
-//     acquires the pulock (process-level file lock) for atomic read-modify-write.
+//  3. The actual checkpoint mutations happen in driver callbacks which properly
+//     acquire the pulock (process-level file lock) for atomic read-modify-write.
 //  4. Holding DeviceState lock during the entire cleanup (which could take seconds with
 //     multiple API calls) would unnecessarily block normal Prepare/Unprepare operations.
 func (m *CheckpointCleanupManager) cleanup(ctx context.Context) {
@@ -126,6 +130,15 @@ func (m *CheckpointCleanupManager) cleanup(ctx context.Context) {
 
 	for cpuid, cpclaim := range filtered {
 		m.unprepareIfStale(ctx, cpuid, cpclaim)
+	}
+
+	expiredEntries, err := m.expiredEntryCleanupFn(ctx, time.Now(), UnpreparedClaimEntryTTL)
+	if err != nil {
+		klog.Warningf("Checkpointed RC cleanup: unable to delete expired unprepared claim entries: %s", err)
+		return
+	}
+	if expiredEntries > 0 {
+		klog.V(4).Infof("Checkpointed RC cleanup: deleted expired unprepared claim entries: %d", expiredEntries)
 	}
 }
 

cmd/compute-domain-kubelet-plugin/device_state_test.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"encoding/json"
 	"testing"
+	"time"
 
 	resourceapi "k8s.io/api/resource/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -406,6 +407,115 @@ func TestPrepareReturnsCheckpointedDevicesForCompletedClaim(t *testing.T) {
 	assert.Equal(t, []kubeletplugin.Device{kubeletplugin.Device(expectedDevice)}, got)
 }
 
+func TestPrepareRejectsMatchingUnpreparedEntry(t *testing.T) {
+	now := metav1.Now()
+	claim := claimWithResults("claim-uid", allocationResult("request", DriverName, "channel-0", nil))
+	claim.ResourceVersion = "rv-1"
+	checkpoint := checkpointWithClaims(map[string]PreparedClaim{
+		"claim-uid": {
+			CheckpointState: ClaimCheckpointStateUnprepareCompleted,
+			Status:          claim.Status,
+			Name:            claim.Name,
+			Namespace:       claim.Namespace,
+			ResourceVersion: claim.ResourceVersion,
+			UnpreparedAt:    &now,
+		},
+	})
+	state := &DeviceState{
+		checkpointManager: &fakeCheckpointManager{checkpoint: checkpoint},
+	}
+
+	got, err := state.Prepare(context.Background(), claim)
+
+	require.Error(t, err)
+	assert.True(t, isPermanentError(err))
+	assert.Contains(t, err.Error(), "claim was already unprepared")
+	assert.Nil(t, got)
+	assert.Equal(t, ClaimCheckpointStateUnprepareCompleted, requireFakeCheckpointManager(t, state).checkpoint.V2.PreparedClaims["claim-uid"].CheckpointState)
+}
+
+func TestClaimMatchesPreparedClaim(t *testing.T) {
+	claim := claimWithResults("claim-uid", allocationResult("request", DriverName, "channel-0", nil))
+	claim.ResourceVersion = "rv-1"
+
+	assert.True(t, claimMatchesPreparedClaim(PreparedClaim{
+		Status:          claim.Status,
+		ResourceVersion: "rv-1",
+	}, claim))
+
+	assert.False(t, claimMatchesPreparedClaim(PreparedClaim{
+		Status:          claim.Status,
+		ResourceVersion: "rv-2",
+	}, claim))
+
+	claim.ResourceVersion = ""
+	assert.True(t, claimMatchesPreparedClaim(PreparedClaim{
+		Status: claim.Status,
+	}, claim))
+}
+
+func TestMarkClaimUnpreparedInCheckpointWritesEntry(t *testing.T) {
+	checkpoint := checkpointWithClaims(map[string]PreparedClaim{
+		"claim-uid": {
+			CheckpointState: ClaimCheckpointStatePrepareCompleted,
+			Status:          claimStatus([]resourceapi.DeviceRequestAllocationResult{allocationResult("request", DriverName, "channel-0", nil)}),
+			PreparedDevices: PreparedDevices{
+				{Devices: PreparedDeviceList{preparedChannel(0)}},
+			},
+			ResourceVersion: "rv-1",
+		},
+	})
+	state := testCheckpointDeviceState(checkpoint)
+	claimRef := kubeletplugin.NamespacedObject{
+		NamespacedName: types.NamespacedName{
+			Namespace: "default",
+			Name:      "claim",
+		},
+		UID: "claim-uid",
+	}
+	pc := checkpoint.V2.PreparedClaims["claim-uid"]
+
+	err := state.markClaimUnpreparedInCheckpoint(claimRef, pc)
+
+	require.NoError(t, err)
+	stored := requireFakeCheckpointManager(t, state).checkpoint.V2.PreparedClaims["claim-uid"]
+	assert.Equal(t, ClaimCheckpointStateUnprepareCompleted, stored.CheckpointState)
+	assert.Equal(t, "claim", stored.Name)
+	assert.Equal(t, "default", stored.Namespace)
+	assert.Equal(t, "rv-1", stored.ResourceVersion)
+	assert.Nil(t, stored.PreparedDevices)
+	require.NotNil(t, stored.UnpreparedAt)
+}
+
+func TestDeleteExpiredUnpreparedClaimsFromCheckpoint(t *testing.T) {
+	now := time.Now()
+	old := metav1.NewTime(now.Add(-UnpreparedClaimEntryTTL - time.Second))
+	fresh := metav1.NewTime(now.Add(-UnpreparedClaimEntryTTL + time.Second))
+	checkpoint := checkpointWithClaims(map[string]PreparedClaim{
+		"old": {
+			CheckpointState: ClaimCheckpointStateUnprepareCompleted,
+			UnpreparedAt:    &old,
+		},
+		"fresh": {
+			CheckpointState: ClaimCheckpointStateUnprepareCompleted,
+			UnpreparedAt:    &fresh,
+		},
+		"completed": {
+			CheckpointState: ClaimCheckpointStatePrepareCompleted,
+		},
+	})
+	state := testCheckpointDeviceState(checkpoint)
+
+	deleted, err := state.deleteExpiredUnpreparedClaimsFromCheckpoint(now, UnpreparedClaimEntryTTL)
+
+	require.NoError(t, err)
+	assert.Equal(t, 1, deleted)
+	stored := requireFakeCheckpointManager(t, state).checkpoint.V2.PreparedClaims
+	assert.NotContains(t, stored, "old")
+	assert.Contains(t, stored, "fresh")
+	assert.Contains(t, stored, "completed")
+}
+
 func TestUnprepareMissingClaimIsNoop(t *testing.T) {
 	state := &DeviceState{
 		checkpointManager: &fakeCheckpointManager{checkpoint: checkpointWithClaims(nil)},
@@ -430,6 +540,23 @@ func testDeviceState() *DeviceState {
 			"daemon-0":  &AllocatableDevice{Daemon: &ComputeDomainDaemonInfo{ID: 0}},
 		},
 	}
+}
+
+func testCheckpointDeviceState(checkpoint *Checkpoint) *DeviceState {
+	return &DeviceState{
+		checkpointManager: &fakeCheckpointManager{checkpoint: checkpoint},
+		config: &Config{
+			flags: &Flags{nodeName: "test-node"},
+		},
+	}
+}
+
+func requireFakeCheckpointManager(t *testing.T, state *DeviceState) *fakeCheckpointManager {
+	t.Helper()
+
+	manager, ok := state.checkpointManager.(*fakeCheckpointManager)
+	require.True(t, ok)
+	return manager
 }
 
 func channelConfig(domainID string) *configapi.ComputeDomainChannelConfig {

cmd/compute-domain-kubelet-plugin/driver.go
@@ -123,8 +123,8 @@ func NewDriver(ctx context.Context, config *Config) (*driver, error) {
 		return nil, fmt.Errorf("error starting ComputeDomain manager: %w", err)
 	}
 
-	// Pass `nodeUnprepareResource` function in the cleanup manager.
-	if err := state.checkpointCleanupManager.Start(ctx, driver.nodeUnprepareResource); err != nil {
+	// Pass checkpoint mutation functions to the cleanup manager.
+	if err := state.checkpointCleanupManager.Start(ctx, driver.nodeUnprepareResource, driver.deleteExpiredUnpreparedClaimEntries); err != nil {
 		return nil, fmt.Errorf("error starting CheckpointCleanupManager: %w", err)
 	}
 
@@ -304,6 +304,16 @@ func (d *driver) nodeUnprepareResource(ctx context.Context, claimRef kubeletplug
 	klog.V(1).Infof("Unprepared devices for claim '%v'", claimRef.String())
 	drametrics.ObserveRequest(DriverName, "unprepare", time.Since(tstart))
 	return true, nil
+}
+
+func (d *driver) deleteExpiredUnpreparedClaimEntries(ctx context.Context, now time.Time, ttl time.Duration) (int, error) {
+	release, err := d.pulock.Acquire(ctx, flock.WithTimeout(10*time.Second))
+	if err != nil {
+		return 0, fmt.Errorf("error acquiring prep/unprep lock: %w", err)
+	}
+	defer release()
+
+	return d.state.deleteExpiredUnpreparedClaimsFromCheckpoint(now, ttl)
 }
 
 // TODO: implement loop to remove CDI files from the CDI path for claimUIDs

varunrsekar/k8s-dra-driver

cmd/compute-domain-kubelet-plugin/cleanup.go
@@ -33,9 +33,11 @@ import (
 
 const (
 	ResourceClaimCleanupInterval = 10 * time.Minute
+	PrepareAbortedClaimEntryTTL  = 2 * ErrorRetryMaxTimeout
 )
 
 type TypeUnprepCallable = func(ctx context.Context, claimRef kubeletplugin.NamespacedObject) (bool, error)
+type TypeExpiredEntryCleanupCallable = func(ctx context.Context, now time.Time, ttl time.Duration) (int, error)
 
 type CheckpointCleanupManager struct {
 	waitGroup     sync.WaitGroup
@@ -44,7 +46,8 @@ type CheckpointCleanupManager struct {
 	devicestate   *DeviceState
 	draclient     *draclient.Client
 
-	unprepfunc TypeUnprepCallable
+	unprepfunc            TypeUnprepCallable
+	expiredEntryCleanupFn TypeExpiredEntryCleanupCallable
 }
 
 func NewCheckpointCleanupManager(s *DeviceState, client *draclient.Client) *CheckpointCleanupManager {
@@ -58,10 +61,11 @@ func NewCheckpointCleanupManager(s *DeviceState, client *draclient.Client) *Chec
 	}
 }
 
-func (m *CheckpointCleanupManager) Start(ctx context.Context, unprepfunc TypeUnprepCallable) error {
+func (m *CheckpointCleanupManager) Start(ctx context.Context, unprepfunc TypeUnprepCallable, expiredEntryCleanupFn TypeExpiredEntryCleanupCallable) error {
 	ctx, cancel := context.WithCancel(ctx)
 	m.cancelContext = cancel
 	m.unprepfunc = unprepfunc
+	m.expiredEntryCleanupFn = expiredEntryCleanupFn
 
 	m.waitGroup.Add(1)
 	go func() {
@@ -91,10 +95,10 @@ func (m *CheckpointCleanupManager) Stop() error {
 
 // cleanup() is the high-level cleanup routine run once upon plugin startup and
 // then periodically. It gets all claims in PrepareStarted state from the
-// current checkpoint, and runs `unprepareIfStale()` for each of them. Each
-// invocation of `cleanup()` and each invocation of `unprepareIfStale()` is
-// best-effort: errors do not need to be propagated (but are expected to be
-// properly logged).
+// current checkpoint, and runs `unprepareIfStale()` for each of them. It also
+// removes expired PrepareAborted entries. Each invocation of `cleanup()`
+// and each invocation of `unprepareIfStale()` is best-effort: errors do not
+// need to be propagated (but are expected to be properly logged).
 //
 // Note: This function does not acquire DeviceState lock when reading the checkpoint.
 // The lock is not needed because:
@@ -103,8 +107,8 @@ func (m *CheckpointCleanupManager) Stop() error {
 //     means we catch it on the next periodic iteration.
 //  2. We validate each candidate against the API server (the authoritative source of
 //     truth for whether a claim is truly stale), not the local checkpoint state.
-//  3. The actual checkpoint mutation happens in nodeUnprepareResource(), which properly
-//     acquires the pulock (process-level file lock) for atomic read-modify-write.
+//  3. The actual checkpoint mutations happen in driver callbacks which properly
+//     acquire the pulock (process-level file lock) for atomic read-modify-write.
 //  4. Holding DeviceState lock during the entire cleanup (which could take seconds with
 //     multiple API calls) would unnecessarily block normal Prepare/Unprepare operations.
 func (m *CheckpointCleanupManager) cleanup(ctx context.Context) {
@@ -126,6 +130,15 @@ func (m *CheckpointCleanupManager) cleanup(ctx context.Context) {
 
 	for cpuid, cpclaim := range filtered {
 		m.unprepareIfStale(ctx, cpuid, cpclaim)
+	}
+
+	expiredEntries, err := m.expiredEntryCleanupFn(ctx, time.Now(), PrepareAbortedClaimEntryTTL)
+	if err != nil {
+		klog.Warningf("Checkpointed RC cleanup: unable to delete expired PrepareAborted claim entries: %s", err)
+		return
+	}
+	if expiredEntries > 0 {
+		klog.V(4).Infof("Checkpointed RC cleanup: deleted expired PrepareAborted claim entries: %d", expiredEntries)
 	}
 }
 

cmd/compute-domain-kubelet-plugin/device_state_test.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"encoding/json"
 	"testing"
+	"time"
 
 	resourceapi "k8s.io/api/resource/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -406,6 +407,126 @@ func TestPrepareReturnsCheckpointedDevicesForCompletedClaim(t *testing.T) {
 	assert.Equal(t, []kubeletplugin.Device{kubeletplugin.Device(expectedDevice)}, got)
 }
 
+func TestPrepareRejectsMatchingPrepareAbortedEntry(t *testing.T) {
+	now := metav1.Now()
+	claim := claimWithResults("claim-uid", allocationResult("request", DriverName, "channel-0", nil))
+	checkpoint := checkpointWithClaims(map[string]PreparedClaim{
+		"claim-uid": {
+			CheckpointState: ClaimCheckpointStatePrepareAborted,
+			Status:          claim.Status,
+			Name:            claim.Name,
+			Namespace:       claim.Namespace,
+			AbortedAt:       &now,
+		},
+	})
+	state := &DeviceState{
+		checkpointManager: &fakeCheckpointManager{checkpoint: checkpoint},
+	}
+
+	got, err := state.Prepare(context.Background(), claim)
+
+	require.Error(t, err)
+	assert.True(t, isPermanentError(err))
+	assert.Contains(t, err.Error(), "claim prepare was already aborted")
+	assert.Nil(t, got)
+	assert.Equal(t, ClaimCheckpointStatePrepareAborted, requireFakeCheckpointManager(t, state).checkpoint.V2.PreparedClaims["claim-uid"].CheckpointState)
+}
+
+func TestClaimMatchesPreparedClaim(t *testing.T) {
+	claim := claimWithResults("claim-uid", allocationResult("request", DriverName, "channel-0", nil))
+
+	assert.True(t, claimMatchesPreparedClaim(PreparedClaim{
+		Status: claim.Status,
+	}, claim))
+
+	assert.False(t, claimMatchesPreparedClaim(PreparedClaim{
+		Status: claimStatus([]resourceapi.DeviceRequestAllocationResult{
+			allocationResult("request", DriverName, "daemon-0", nil),
+		}),
+	}, claim))
+}
+
+func TestMarkClaimPrepareAbortedInCheckpointWritesEntry(t *testing.T) {
+	checkpoint := checkpointWithClaims(map[string]PreparedClaim{
+		"claim-uid": {
+			CheckpointState: ClaimCheckpointStatePrepareStarted,
+			Status:          claimStatus([]resourceapi.DeviceRequestAllocationResult{allocationResult("request", DriverName, "channel-0", nil)}),
+			PreparedDevices: PreparedDevices{
+				{Devices: PreparedDeviceList{preparedChannel(0)}},
+			},
+		},
+	})
+	state := testCheckpointDeviceState(checkpoint)
+	claimRef := kubeletplugin.NamespacedObject{
+		NamespacedName: types.NamespacedName{
+			Namespace: "default",
+			Name:      "claim",
+		},
+		UID: "claim-uid",
+	}
+	pc := checkpoint.V2.PreparedClaims["claim-uid"]
+
+	err := state.markClaimPrepareAbortedInCheckpoint(claimRef, pc)
+
+	require.NoError(t, err)
+	stored := requireFakeCheckpointManager(t, state).checkpoint.V2.PreparedClaims["claim-uid"]
+	assert.Equal(t, ClaimCheckpointStatePrepareAborted, stored.CheckpointState)
+	assert.Equal(t, "claim", stored.Name)
+	assert.Equal(t, "default", stored.Namespace)
+	assert.Nil(t, stored.PreparedDevices)
+	require.NotNil(t, stored.AbortedAt)
+}
+
+func TestDeleteClaimFromCheckpoint(t *testing.T) {
+	checkpoint := checkpointWithClaims(map[string]PreparedClaim{
+		"claim-uid": {
+			CheckpointState: ClaimCheckpointStatePrepareCompleted,
+		},
+	})
+	state := testCheckpointDeviceState(checkpoint)
+	claimRef := kubeletplugin.NamespacedObject{
+		NamespacedName: types.NamespacedName{
+			Namespace: "default",
+			Name:      "claim",
+		},
+		UID: "claim-uid",
+	}
+
+	err := state.deleteClaimFromCheckpoint(claimRef)
+
+	require.NoError(t, err)
+	assert.NotContains(t, requireFakeCheckpointManager(t, state).checkpoint.V2.PreparedClaims, "claim-uid")
+}
+
+func TestDeleteExpiredPrepareAbortedClaimsFromCheckpoint(t *testing.T) {
+	now := time.Now()
+	old := metav1.NewTime(now.Add(-PrepareAbortedClaimEntryTTL - time.Second))
+	fresh := metav1.NewTime(now.Add(-PrepareAbortedClaimEntryTTL + time.Second))
+	checkpoint := checkpointWithClaims(map[string]PreparedClaim{
+		"old": {
+			CheckpointState: ClaimCheckpointStatePrepareAborted,
+			AbortedAt:       &old,
+		},
+		"fresh": {
+			CheckpointState: ClaimCheckpointStatePrepareAborted,
+			AbortedAt:       &fresh,
+		},
+		"completed": {
+			CheckpointState: ClaimCheckpointStatePrepareCompleted,
+		},
+	})
+	state := testCheckpointDeviceState(checkpoint)
+
+	deleted, err := state.deleteExpiredPrepareAbortedClaimsFromCheckpoint(now, PrepareAbortedClaimEntryTTL)
+
+	require.NoError(t, err)
+	assert.Equal(t, 1, deleted)
+	stored := requireFakeCheckpointManager(t, state).checkpoint.V2.PreparedClaims
+	assert.NotContains(t, stored, "old")
+	assert.Contains(t, stored, "fresh")
+	assert.Contains(t, stored, "completed")
+}
+
 func TestUnprepareMissingClaimIsNoop(t *testing.T) {
 	state := &DeviceState{
 		checkpointManager: &fakeCheckpointManager{checkpoint: checkpointWithClaims(nil)},
@@ -430,6 +551,23 @@ func testDeviceState() *DeviceState {
 			"daemon-0":  &AllocatableDevice{Daemon: &ComputeDomainDaemonInfo{ID: 0}},
 		},
 	}
+}
+
+func testCheckpointDeviceState(checkpoint *Checkpoint) *DeviceState {
+	return &DeviceState{
+		checkpointManager: &fakeCheckpointManager{checkpoint: checkpoint},
+		config: &Config{
+			flags: &Flags{nodeName: "test-node"},
+		},
+	}
+}
+
+func requireFakeCheckpointManager(t *testing.T, state *DeviceState) *fakeCheckpointManager {
+	t.Helper()
+
+	manager, ok := state.checkpointManager.(*fakeCheckpointManager)
+	require.True(t, ok)
+	return manager
 }
 
 func channelConfig(domainID string) *configapi.ComputeDomainChannelConfig {

cmd/compute-domain-kubelet-plugin/driver.go
@@ -123,8 +123,8 @@ func NewDriver(ctx context.Context, config *Config) (*driver, error) {
 		return nil, fmt.Errorf("error starting ComputeDomain manager: %w", err)
 	}
 
-	// Pass `nodeUnprepareResource` function in the cleanup manager.
-	if err := state.checkpointCleanupManager.Start(ctx, driver.nodeUnprepareResource); err != nil {
+	// Pass checkpoint mutation functions to the cleanup manager.
+	if err := state.checkpointCleanupManager.Start(ctx, driver.nodeUnprepareResource, driver.deleteExpiredPrepareAbortedClaimEntries); err != nil {
 		return nil, fmt.Errorf("error starting CheckpointCleanupManager: %w", err)
 	}
 
@@ -304,6 +304,16 @@ func (d *driver) nodeUnprepareResource(ctx context.Context, claimRef kubeletplug
 	klog.V(1).Infof("Unprepared devices for claim '%v'", claimRef.String())
 	drametrics.ObserveRequest(DriverName, "unprepare", time.Since(tstart))
 	return true, nil
+}
+
+func (d *driver) deleteExpiredPrepareAbortedClaimEntries(ctx context.Context, now time.Time, ttl time.Duration) (int, error) {
+	release, err := d.pulock.Acquire(ctx, flock.WithTimeout(10*time.Second))
+	if err != nil {
+		return 0, fmt.Errorf("error acquiring prep/unprep lock: %w", err)
+	}
+	defer release()
+
+	return d.state.deleteExpiredPrepareAbortedClaimsFromCheckpoint(now, ttl)
 }
 
 // TODO: implement loop to remove CDI files from the CDI path for claimUIDs